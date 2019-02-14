d28284684009ebd13f6a28e7db4370e0fe685229
ivanthelad <ivanthelad>
2019-02-14 14:23:13 +0100

    quicktest

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ class App extends Component {
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
-9999922222            Edit <code>src/App.js</code> and save to reload.
+Hello Daimler Edit <code>src/App.js</code> and save to reload.
           </p>
           <a
             className="App-link"
